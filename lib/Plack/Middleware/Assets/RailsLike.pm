081b0a11092262fddf41c42f0509518e4280731c
ysasaki <aloelight@gmail.com>
2013-07-01 23:23:31 +0900

    Checking in changes prior to tagging of version 0.09.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 6dc7a95..bb4a7f7 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,6 +1,8 @@
     Revision history for Perl extension Plack-Middleware-Assets-RailsLike
    
     {{$NEXT}}
    +
    +0.09 2013-07-01T14:23:25Z
         - Uses Text::Sass::XS.
         - Disables caching if PLACK_ENV equals "development" and didn't pass a cache object.

--- lib/Plack/Middleware/Assets/RailsLike.pm
+++ lib/Plack/Middleware/Assets/RailsLike.pm
@@ -16,7 +16,7 @@ use HTTP::Date ();
 use Plack::Util::Accessor qw(path root search_path expires cache minify);
 use Plack::Middleware::Assets::RailsLike::Compiler;
 
-our $VERSION = "0.08";
+our $VERSION = "0.09";
 
 our $EXPIRES_NEVER = $Cache::Cache::EXPIRES_NEVER;
 our $EXPIRES_NOW   = $Cache::Cache::EXPIRES_NOW;
